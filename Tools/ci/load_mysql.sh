5100afe09193de3a7d6ca268e50a0d5513886d7e
hawon6691 <hawonoj002@gmail.com>
2026-04-24 01:59:01 +0900

    fix: (#7) bootstrap mysql ci databases with root

--- Tools/ci/load_mysql.sh
+++ Tools/ci/load_mysql.sh
@@ -9,6 +9,8 @@ host="${MYSQL_HOST:-127.0.0.1}"
 port="${MYSQL_PORT:-3306}"
 user="${MYSQL_USER:-project_bible}"
 password="${MYSQL_PASSWORD:-project_bible}"
+bootstrap_user="${MYSQL_BOOTSTRAP_USER:-root}"
+bootstrap_password="${MYSQL_ROOT_PASSWORD:-${password}}"
 
 schema_path="Database/mysql/${domain}/init/01_schema.sql"
 seed_path="Database/mysql/${domain}/seeds/01_seed.sql"
@@ -23,6 +25,6 @@ if [[ ! -f "${seed_path}" ]]; then
   exit 1
 fi
 
-mysql --protocol=TCP -h "${host}" -P "${port}" -u "${user}" "-p${password}" -e "DROP DATABASE IF EXISTS \`${db_name}\`; CREATE DATABASE \`${db_name}\`;"
-mysql --protocol=TCP -h "${host}" -P "${port}" -u "${user}" "-p${password}" "${db_name}" < "${schema_path}"
-mysql --protocol=TCP -h "${host}" -P "${port}" -u "${user}" "-p${password}" "${db_name}" < "${seed_path}"
+mysql --protocol=TCP -h "${host}" -P "${port}" -u "${bootstrap_user}" "-p${bootstrap_password}" -e "DROP DATABASE IF EXISTS \`${db_name}\`; CREATE DATABASE \`${db_name}\`; CREATE USER IF NOT EXISTS '${user}'@'%' IDENTIFIED BY '${password}'; GRANT ALL PRIVILEGES ON \`${db_name}\`.* TO '${user}'@'%'; FLUSH PRIVILEGES;"
+mysql --protocol=TCP -h "${host}" -P "${port}" -u "${bootstrap_user}" "-p${bootstrap_password}" "${db_name}" < "${schema_path}"
+mysql --protocol=TCP -h "${host}" -P "${port}" -u "${bootstrap_user}" "-p${bootstrap_password}" "${db_name}" < "${seed_path}"
